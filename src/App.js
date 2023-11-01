96828654831a57cfac0803d2da6537767805c89f
Trend20 <enockomondi305@gmail.com>
2023-11-01 18:23:07 +0300

    auth component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <div className="App">
       <h1>React Firebase Demo App</h1>
+      <Auth />
     </div>
   );
 }
